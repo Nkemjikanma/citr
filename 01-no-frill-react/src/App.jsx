5bdcd8814105cf658aa53a6dbfc221b22a22d7c7
Nkemjikanma <57384968+Nkemjikanma@users.noreply.github.com>
2023-01-28 22:37:21 +0000

    set up jsx and create pet component

--- 01-no-frill-react/src/App.jsx
+++ 01-no-frill-react/src/App.jsx
@@ -1,36 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom";
 
+import Pet from "./Pet";
+
 // the app creates a div with a child h1 within it. The {} will take whatever attributes we pass to the element being created; like id, className
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Parrot",
-    }),
-    React.createElement(Pet, {
-      name: "Jolly",
-      animal: "Dog",
-      breed: "German Shephard",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="cat" breed="Parrot" />
+      <Pet name="Babs" animal="Dog" breed="Mixed" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot() is new with React 18 that has concurrency
-root.render(React.createElement(App)); // react will render the function App which creates elements
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+root.render(<App />); // react will render the function App which creates elements
