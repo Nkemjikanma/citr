570b6f69f916ba787de39ba07e08eae47b045374
Nkemjikanma <57384968+Nkemjikanma@users.noreply.github.com>
2023-01-30 12:25:09 +0000

    add useParams to Details component and use the header tag in App-jsx

--- 01-no-frill-react/src/Details.jsx
+++ 01-no-frill-react/src/Details.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 const Details = () => {
+  const { id } = useParams();
+  console.log(id);
   return (
     <div>
-      <h1>Hi</h1>
+      <h1>{id}</h1>
     </div>
   );
 };
